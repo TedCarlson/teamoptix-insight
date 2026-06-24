09170e26b821d1fe903b02dc2444fd7d69d1e889
Ted Carlson <ted.carlson@itgcomm.com>
2026-06-24 16:39:14 -0400

    v1.6.24.07 > Use Playwright dispatchEvent for DSW export

--- apps/automation-worker/src/fedex-download.ts
+++ apps/automation-worker/src/fedex-download.ts
@@ -344,11 +344,9 @@ export async function downloadDswExcel(input: {
 
     let filesystemDownload;
     try {
-      await excelIcon.evaluate((el) => {
-        el.dispatchEvent(new MouseEvent("mousedown", { bubbles: true, cancelable: true, view: window }));
-        el.dispatchEvent(new MouseEvent("mouseup", { bubbles: true, cancelable: true, view: window }));
-        el.dispatchEvent(new MouseEvent("click", { bubbles: true, cancelable: true, view: window }));
-      });
+      await excelIcon.dispatchEvent("mousedown");
+      await excelIcon.dispatchEvent("mouseup");
+      await excelIcon.dispatchEvent("click");
       filesystemDownload = await waitForCompletedDownload({
         downloadDir: DOWNLOAD_DIR,
         beforePaths: beforeDswDownloads,
