a5f6b10589e350d7a33582d273808bded3796f4b
Ted Carlson <ted.carlson@itgcomm.com>
2026-06-29 10:13:00 -0400

    v1.6.29.08 > Align operations pulse request constraint

--- supabase/migrations/20260629140500_operations_pulse_request_type.sql
+++ supabase/migrations/20260629140500_operations_pulse_request_type.sql
@@ -1,8 +1,8 @@
 alter table core.operations_collection_request
-  drop constraint if exists operations_collection_request_request_type_check;
+  drop constraint if exists operations_collection_request_type_chk;
 
 alter table core.operations_collection_request
-  add constraint operations_collection_request_request_type_check
+  add constraint operations_collection_request_type_chk
   check (
     request_type in (
       'PREVIOUS_DAY_CLOSE',
